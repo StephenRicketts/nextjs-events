52e39c52810a198a6ba195151800339580c43fd2
StephenRicketts <stephen.ricketts12@gmail.com>
2023-11-28 22:12:40 -0400

    added event components and started with even search

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
 import { getFeaturedEvents } from "@/dummy-data";
 import Head from "next/head";
 
 export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
+
   return (
     <>
       <Head>
         <title>events-project</title>
       </Head>
       <main>
+        <EventsSearch />
         <EventList items={featuredEvents} />
       </main>
     </>
